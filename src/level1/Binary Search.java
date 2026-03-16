1e4e3daa2e58b7fdf7f2fcb7b84ead1b25710435
thurtado27 <155494987+thurtado27@users.noreply.github.com>
2026-03-16 01:31:58 +0000

    Added binary search for score lookup

--- src/level1/Binary Search.java
+++ src/level1/Binary Search.java
@@ -1,3 +1,7 @@
+/**
+ * Sorts the given array of scores in descending order using selection sort.
+ * The largest remaining element is moved to the front in each iteration.
+ */
 public void selectionsSortScores(int[] scores) {
     for (int i = 0; i < scores.length -1; i++) {
         int maxIndex = i;
